1f4b83ea747cebc9a75fab563c8b021dcfc78fd9
DevanasDeveloper <devanas.anas@gmail.com>
2025-02-15 00:44:36 +0100

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor/categories',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '577d145ce64ef23c0841d77e6ad73e88ac55a479',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor/categories' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '577d145ce64ef23c0841d77e6ad73e88ac55a479',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
